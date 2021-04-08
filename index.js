86612fe23fea05cabfa3d612e46df2d1bc4ef628
Avraz  Zebary <avrazebary@gmail.com>
2021-04-08 13:21:53 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,33 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString){
+ let this_time = timeString.split(":");
+ let hour = this_time[0];
+ let minute = this_time[1];
+  
+ let this_hour = parseInt(hour);
+ let this_minute = parseInt(minute);
+ 
+  
+  if(this_hour<12){
+    return "Good Morning";
+  }
+  else if((this_hour > 12) && (this_hour<17) ){
+     return "Good Afternoon";
+  }
+  else if (this_hour>17){
+    return "Good Evening";
+  }
+  
+  else 
+  return null;
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greeting){
+  
+   document.getElementById("greeting").innerText = greeting;
+  
+}
