3e5f2c29d8235e69a3630a616aac142a05258612
eman hamid <emanhharki@gmail.com>
2021-04-08 12:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 /* Given Code, don't edit */
 
 document.querySelector("button").addEventListener("click", handleClick);
@@ -10,4 +14,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let first_time =time.split(":");
+  let second_time=first_time[0].concat(first_time[1]);
+  let result=parseInt(second_time,10);
+
+  if(result<1200){
+    return"Good Morning";
+  }
+  else if((result>=1200)&&(result<=1700)){
+    return"Good Afternoon";
+  }
+  else if (result>1700){
+    return"Good Evening";
+  }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(){
+let node = document.getElementById("greeting");
+node.innerText=("TEST");
+
+}
