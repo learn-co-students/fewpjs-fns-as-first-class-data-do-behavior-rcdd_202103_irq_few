e0641d0a61c2e38e9bba30792bda71be732c609f
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-10 09:00:45 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let timeSplited = timeString.split(':');
+  let timeNumber = parseInt(timeSplited[0]);
+  if (timeNumber < 12){
+    return 'Good Morning';}
+
+  else if (timeNumber >= 12 && timeNumber <= 17){
+    return 'Good Afternoon';}
+
+  else if (timeNumber > 17){return 'Good Evening';}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greet){
+  const greeting = document.getElementById('greeting');
+  greeting.innerText = greet;
+}
