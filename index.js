586ce49d9bd35431ca1de48a71cc9e2316f7ceb5
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 11:33:10 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet( str){
+  if (parseInt (str) <12) {
+    return "Good Morning"
+
+  } else if(parseInt (str)>12 && parseInt (str)<17){
+    return "Good Afternoon"
+
+  }
+  else if(parseInt (str)>17){
+    return "Good Evening"
+  }
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str){
+  const h1 = document.getElementById('greeting');
+  h1.innerText= str;
+}
