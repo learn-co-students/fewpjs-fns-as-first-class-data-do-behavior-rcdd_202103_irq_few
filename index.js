673ab213899051c0c985419cbc006c60fc8c92b0
Yusra Ahmed <isra.00287446@gmail.com>
2021-05-15 23:49:39 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let t=time.split(":");
+  
+  if(t[0]<12){
+    return "Good Morning";
+  }
+  else if(t[0]>=12 && t[0]<=17){
+     return "Good Afternoon";
+  }
+  else if(t[0]>17 && t[0]<24){
+       return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  document.getElementById("greeting").innerText=message;
+}
